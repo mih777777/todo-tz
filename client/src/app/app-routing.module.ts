a6225c88dc29793ea2bd8742a3d1bd704f288709
mih777 <mih777777@yandex.ru>
2020-03-21 18:33:23 +0300

    Страницы созданы

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CreateTodoComponent } from './components/create-todo/create-todo.component';
+import { UpdateTodoComponent } from './components/update-todo/update-todo.component';
+import { MainComponent } from './components/main/main.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MainComponent },
+  { path: 'create', component: CreateTodoComponent },
+  { path: 'update/:id', component: UpdateTodoComponent }
+  
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateTodoComponent } from './components/create-todo/create-todo.component';
+import { UpdateTodoComponent } from './components/update-todo/update-todo.component';
+import { MainComponent } from './components/main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateTodoComponent,
+    UpdateTodoComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
