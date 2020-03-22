a51b01e0091c49e24f3653e1ce9e2ef700f50612
mih777 <mih777777@yandex.ru>
2020-03-22 15:24:53 +0300

    Добавляются задания

--- client/src/app/components/create-todo/create-todo.component.ts
+++ client/src/app/components/create-todo/create-todo.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
+
+
+import { TodoServiceService } from 'src/app/services/todo-service.service';
+import { Todo } from 'src/app/interfaces';
+
 
 @Component({
   selector: 'app-create-todo',
@@ -7,9 +14,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateTodoComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup
+  todos: Todo[] = []
+
+  constructor(
+    private todoService: TodoServiceService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.form = new FormGroup({
+      title: new FormControl(''),
+      category: new FormControl(''),
+      description: new FormControl(''),
+
+    })
+  }
+
+  createTodo() {
+    
+    const formData = {...this.form.value}
+
+    this.todoService.create({
+      title: formData.title,
+      category: formData.category,
+      description: formData.description,
+      created_date: formData.created_date
+    }).subscribe(todo => {
+      this.todos.push(todo)
+      this.form.reset()
+      this.router.navigate(['/'])
+
+    })
   }
 
 }
