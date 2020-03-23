782bcdc401dc173f09a2513bfbee9b2381a8df40
mih777 <mih777777@yandex.ru>
2020-03-23 22:49:49 +0300

    Алгаритм почти готов

--- client/src/app/components/main/main.component.ts
+++ client/src/app/components/main/main.component.ts
@@ -8,19 +8,37 @@ import { Todo } from '../../interfaces'
   styleUrls: ['./main.component.scss']
 })
 export class MainComponent implements OnInit {
+  
+  arrayId = []
+  //isChecked: boolean = false
 
   todos: Todo[] = []
 
   constructor(private todoService: TodoServiceService) { }
+  
+  changeIsChecked(id: string) {
+    let res = this.arrayId.indexOf(id)
+    
+    if(res == -1){
+      this.arrayId.push(id)
+    } else {
+      this.arrayId.splice(res, 1)
+    }
+
+    console.log(this.arrayId)
+  }
+
 
   ngOnInit(): void {
     this.fetchAllTodos()
+    
   }
 
   fetchAllTodos(){
     this.todoService.getAll()
       .subscribe(result => {
         this.todos = result
+        //console.log(this.todos)
       })
   }
 
